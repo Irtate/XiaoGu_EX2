2fd789df747438df9aba4431ef6a1d7f0f2a066d
Code-Shy <2953190840@qq.com>
2022-03-15 17:30:30 +0800

    Create DP

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-def print_hi(name):
-    print(f'Hi, {name}')
-
+from src.dp import dp
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    with open('data/test/beibao0.in') as file_object:
+        lines = file_object.readlines()
 
+    v = [0] * (len(lines))
+    w = [0] * (len(lines))
+    V, N = list(map(int, lines[0].rstrip().split()))
+    for i in range(1, len(lines)):
+        V_, W_ = list(map(int, lines[i].rstrip().split()))
+        v[i] = V_
+        w[i] = W_
+
+    dp(N, V, v, w)
--- src/dp.py
+++ src/dp.py
@@ -1,5 +1,15 @@
-import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 
-def dp():
-    pass
+def dp(N, V, v, w):
+    f = [0] * (V + 1)
+
+    for i in range(1, N+1):
+        for j in range(V, v[i]-1,-1):
+            f[j] = max(f[j], f[j-v[i]]+w[i])
+
+    plt.style.use('seaborn')
+    fig, ax = plt.subplots()
+    ax.scatter(v, w, s=10)
+    plt.show()
+
+    print(f[V])
